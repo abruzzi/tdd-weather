78c853b23ff4447696284ab32db1fd273ebc106f
Juntao Qiu <juntao.qiu@gmail.com>
2023-01-25 10:10:09 +1100

    added title

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('weather application', () => {
+  it('renders title', () => {
+    render(<App />);
+    const title = screen.getByText(/Weather Application/i);
+    expect(title).toBeInTheDocument();
+  });
+})
+
