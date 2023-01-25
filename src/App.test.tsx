0190c5af3dca3614d4c8fc1ae7d12ab4281c1c90
Juntao Qiu <juntao.qiu@gmail.com>
2023-01-25 13:51:49 +1100

    added mirage and simple mock

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
 
 describe('weather application', () => {
   it('renders title', () => {
@@ -8,5 +9,17 @@ describe('weather application', () => {
     const title = screen.getByText(/Weather Application/i);
     expect(title).toBeInTheDocument();
   });
+
+  it('shows a list of matches when search', async () => {
+    render(<App />);
+
+    const input = screen.getByTestId('search-input');
+    userEvent.type(input, 'Melbourne');
+
+    const button = screen.getByTestId('search-button');
+    userEvent.click(button);
+
+    await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5));
+  })
 })
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
-import './App.css';
+import React, { ChangeEvent, useState } from "react";
+import "./App.css";
+
+import { createServer } from "miragejs";
+import data from './search-response.json';
+
+createServer({
+  routes() {
+    this.urlPrefix = "https://api.openweathermap.org/geo/1.0";
+
+    this.get("/direct", () => {
+      return data;
+    });
+  },
+});
 
 function App() {
+  const [query, setQuery] = useState<string>("");
+  const [cities, setCities] = useState<string[]>([]);
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setQuery(e.target.value);
+  }
+
+  function handleClick() {
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${query}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCities(data.map((d: any) => `${d.name}, ${d.country}`));
+      });
+  }
+
   return (
     <div className="App">
       <h1>Weather Application</h1>
+      <input
+        type="text"
+        data-testid="search-input"
+        onChange={handleChange}
+        value={query}
+      />
+      <button data-testid="search-button" onClick={handleClick}>
+        Search
+      </button>
+
+      {cities.map((city) => (
+        <div>{city}</div>
+      ))}
     </div>
   );
 }
